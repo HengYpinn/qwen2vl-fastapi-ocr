6b52017145d6a94f0169f20f5d608be1b22dbd62
HengYpinn <117615053+HengYpinn@users.noreply.github.com>
2025-06-08 15:58:51 +0800

    Refind prompts

--- prompts.py
+++ prompts.py
@@ -2,15 +2,15 @@
 
 PROMPTS = {
     "ic": (
-        "Extract JSON with keys cardType,idNumber,name,address,status,religion,gender,expiryDate,documentAuthenticity from Malaysian IC image.expiryDate format DD-MM-YYYY.documentAuthenticity:integer 0–100 representing confidence(%) that the ID is genuine and legible."
+        "Extract JSON with keys cardType(MyKad,MyPR,MyKAS,MyTentera,MyKid),idNumber,name,address,status,religion,gender,expiryDate,blurIntensity(in percentage),glareIntensity(in percentage) from Malaysian IC image.expiryDate format DD-MM-YYYY.Output only JSON."
     ),
 
     "passport": (
-        "Extract JSON with keys fullName,countryCode,passportNumber,nationality,nationalId,dateOfBirth,sex,dateOfIssue,dateOfExpiry,issuedBy,documentAuthenticity from passport scan.documentAuthenticity:integer 0–100 representing confidence(%) that the ID is genuine and legible."
+        "Extract JSON with keys fullName,countryCode,passportNumber,nationality,nationalId,dateOfBirth,sex,dateOfIssue,dateOfExpiry,issuedBy,blurIntensity(in percentage),glareIntensity(in percentage) from passport scan.Output only JSON."
     ),
 
     "receipt": (
         "Extract JSON with keys: if cash deposit receipt -> date,time,accountNumber,name,total,transactionStatus; "
-        "if bank transfer receipt -> status,date,time,amount,referenceCode,toName,toBank,toAccountLast4,transferType,remarks."
+        "if bank transfer receipt -> status,date,time,amount,referenceCode,toName,toBank,toAccountLast4,transferType,remarks.Output only JSON."
     ),
 }
